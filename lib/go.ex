dbc6127c10e398c78e59d4d2daf907167ab3d615
koko le gorille <koko.le.gorille@gmail.com>
2017-04-17 04:35:24 +0200

    Initial commit

---
+++ lib/go.ex
@@ -0,0 +1,18 @@
+defmodule Go do
+  @moduledoc """
+  Documentation for Go.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Go.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/go_test.exs
@@ -0,0 +1,8 @@
+defmodule GoTest do
+  use ExUnit.Case
+  doctest Go
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
